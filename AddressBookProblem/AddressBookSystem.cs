add56c5b9acc99852df0f72d94dd0feebe21ebac
D-Meshach <meshachdaniel456@gmail.com>
2023-05-06 11:08:05 +0530

    Delete Contact Updated

--- AddressBookProblem/AddressBookSystem.cs
+++ AddressBookProblem/AddressBookSystem.cs
@@ -29,7 +29,7 @@ namespace AddressBookProblem
             while (checks == "n")
             {
                 Console.WriteLine("Enter Option");
-                Console.WriteLine("Enter \n 1) Add Single Contact \n 2) DisplayContact");
+                Console.WriteLine("Enter \n 1) Add Single Contact \n 2) DisplayContact \n 3)Edit Contact \n 4) Remove Contact");
                 int option = Convert.ToInt16(Console.ReadLine());
 
                 switch (option) 
@@ -37,6 +37,7 @@ namespace AddressBookProblem
                     case 1:AddSingleContact(); break;
                     case 2:display();break;
                     case 3:EditContact();break;
+                    case 4:DeleteContact();break;
                     default:Console.WriteLine("Worong key");break;
                 }
                 Console.WriteLine("Do you wish to exit??Y/N");
@@ -95,18 +96,42 @@ namespace AddressBookProblem
         }
         public void EditContact()
         {
+            Console.WriteLine("Enter the name to search");
             string name = Convert.ToString(Console.ReadLine());
             AddBook.ToList().Find(u => u.Value.firstName.Contains("asdf"));
             if (AddBook.ContainsKey(name))
             {
-                Console.WriteLine("Update \n 1)lastName \n 2) Address \n 3)city \n 4) state \n  6)Zip \n 7)phone Number \n 8) Email");
+                Console.WriteLine("Update \n 1)lastName \n 2) Address \n 3)city \n 4) state \n  5)Zip \n 6)phone Number \n 7) Email");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:Console.WriteLine("Enter last name");
                         string lastName = Convert.ToString(Console.ReadLine());
-                        
                         AddBook[name].lastName=lastName;break;
+                    case 2:
+                        Console.WriteLine("Enter Address");
+                        string address = Convert.ToString(Console.ReadLine());
+                        AddBook[name].address = address; break;
+                    case 3:
+                        Console.WriteLine("Enter City");
+                        string city = Convert.ToString(Console.ReadLine());
+                        AddBook[name].city = city; break;
+                    case 4:
+                        Console.WriteLine("Enter state");
+                        string state = Convert.ToString(Console.ReadLine());
+                        AddBook[name].state = state; break;
+                    case 5:
+                        Console.WriteLine("Enter zip");
+                        string zip = Convert.ToString(Console.ReadLine());
+                        AddBook[name].zip = zip; break;
+                    case 6:
+                        Console.WriteLine("Enter phone number");
+                        int phoneNumber = Convert.ToInt32(Console.ReadLine());
+                        AddBook[name].phoneNumber = phoneNumber; break;
+                    case 7:
+                        Console.WriteLine("Enter email");
+                        string email = Convert.ToString(Console.ReadLine());
+                        AddBook[name].email = email; break;
                 }
                 
                 /*Console.WriteLine("Enter address");
@@ -124,6 +149,19 @@ namespace AddressBookProblem
                 
                 Console.WriteLine(result.firstName+"--"+result.lastName);
             }*/
+        }
+        public void DeleteContact()
+        {
+            Console.WriteLine("Enter the name to remove");
+            string name = Convert.ToString(Console.ReadLine());
+            if (AddBook.ContainsKey(name))
+            {
+                AddBook.Remove(name);
+            }
+            else 
+            {
+                Console.WriteLine("No such person with first name found");
+            }
         }
 
     }
